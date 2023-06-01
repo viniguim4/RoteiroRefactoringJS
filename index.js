f51f85fbfb4e8accc1408afbfe6e79ae099a6e90
viniguim4 <viniguima@ufmg.br>
2023-06-01 16:16:47 +0000

    Commit 4 - Mais Extract Functions

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const { readFileSync } = require('fs');
 
 function gerarFaturaStr (fatura, pecas) {
 
+  // função extraída
+  function formatarMoeda(valor) {
+    return new Intl.NumberFormat("pt-BR",
+      { style: "currency", currency: "BRL",
+        minimumFractionDigits: 2 }).format(valor/100);
+  }
+
+   // função extraída
+   function calcularCredito(apre) {
+    let creditos = 0;
+    creditos += Math.max(apre.audiencia - 30, 0);
+    if (getPeca(apre).tipo === "comedia") 
+       creditos += Math.floor(apre.audiencia / 5);
+    return creditos;   
+  }
+  
   // função query
   function getPeca(apresentacao) {
     return pecas[apresentacao.id];
@@ -33,9 +49,6 @@ function gerarFaturaStr (fatura, pecas) {
   let totalFatura = 0;
   let creditos = 0;
   let faturaStr = `Fatura ${fatura.cliente}\n`;
-  const formato = new Intl.NumberFormat("pt-BR",
-                        { style: "currency", currency: "BRL",
-                          minimumFractionDigits: 2 }).format;
 
   for (let apre of fatura.apresentacoes) {
     
@@ -43,15 +56,13 @@ function gerarFaturaStr (fatura, pecas) {
     let total = calcularTotalApresentacao(apre);
 
     // créditos para próximas contratações.
-    creditos += Math.max(apre.audiencia - 30, 0);
-    if (getPeca(apre).tipo === "comedia") 
-        creditos += Math.floor(apre.audiencia / 5);
+    creditos += calcularCredito(apre);
 
     // mais uma linha da fatura
-    faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+    faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
     totalFatura += total;
   }
-  faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
+  faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
   faturaStr += `Créditos acumulados: ${creditos} \n`;
   return faturaStr;
 }
